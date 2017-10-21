bbc27fb98714eb0c18f21b8c671250a8df07eed5
aniamoon <ania.sadurska@gmail.com>
2017-10-21 16:28:40 +0200

    zadanie 2

--- js/app.js
+++ js/app.js
@@ -4,10 +4,11 @@ document.addEventListener('DOMContentLoaded', function () {
 var menuElement = document.querySelector('.for-dropdown');
 var dropdownList = document.querySelector('.for-dropdown .dropdown');
 
-menuElement.addEventListener('mouseover', function() {
+menuElement.addEventListener('mouseover', function() 
+
+
+{
 	dropdownList.style.display = 'block';
-
-
 
 });
 
@@ -16,4 +17,34 @@ menuElement.addEventListener('mouseover', function() {
 
 
  });
-});
+
+//zadanie2
+ var buttons = document.querySelectorAll('.btn.read-more');
+    
+
+
+function showHide() {
+        var textArea = this.previousElementSibling;
+
+
+		if (textArea.style.display === 'none' || textArea.style.display === '') {
+		textArea.style.display = 'block';
+		this.innerHTML = 'mniej<span class="glyphicon glyphicon-chevron-up"></span>'
+
+		}
+
+		else {
+			textArea.style.display = 'none';
+			this.innerHTML = 'więcej<span class="glyphicon glyphicon-chevron-up"></span>'
+    };}
+
+for (var i = 0; i < buttons.length; i++) {
+		buttons[i].addEventListener('click', showHide);
+    
+      }
+    });
+
+	
+	
+    
+
