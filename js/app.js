7af7d95f48fe835a94be8cf58507c3ca8bde7412
aniamoon <ania.sadurska@gmail.com>
2017-10-21 16:47:20 +0200

    zadanie 3

--- js/app.js
+++ js/app.js
@@ -42,7 +42,29 @@ for (var i = 0; i < buttons.length; i++) {
 		buttons[i].addEventListener('click', showHide);
     
       }
-    });
+
+
+
+
+   
+//zadanie 3
+	var navigation = document.querySelector('nav.navbar');
+
+	window.addEventListener('scroll', function() {
+		navigation.style.borderBottom = '1px solid #7f7f7f';
+	});
+
+
+
+
+
+
+});
+
+
+	
+
+
 
 	
 	
